9f37baf7bc6853688511159f7c438eb35d1e2e1c
thekill022 <gibfathoni@gmail.com>
2026-05-04 23:42:41 +0700

    membuat variabel untuk mengambil item dari storage

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,3 +1,7 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 class AuthRepository {
   final String baseUrl = 'https://ternak-be-production.up.railway.app/api/v1';
+  final _storage = FlutterSecureStorage();
+
 }
