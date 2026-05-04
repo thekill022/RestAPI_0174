c33582dbed334e801a778eeccc33ae184c65ee76
thekill022 <gibfathoni@gmail.com>
2026-05-05 03:57:53 +0700

    menambahkan controller dan pre fill data hewan pada init state edit page

--- lib/ui/pages/edit_hewan_page.dart
+++ lib/ui/pages/edit_hewan_page.dart
@@ -10,6 +10,32 @@ class EditHewanPage extends StatefulWidget {
 }
 
 class _EditHewanPageState extends State<EditHewanPage> {
+  final _formKey = GlobalKey<FormState>();
+  late final TextEditingController _namaController;
+  late final TextEditingController _jenisController;
+  late final TextEditingController _tanggalLahirController;
+  late final TextEditingController _hargaController;
+  String? _status;
+
+  @override
+  void initState() {
+    super.initState();
+    _namaController = TextEditingController(text: widget.hewan.nama);
+    _jenisController = TextEditingController(text: widget.hewan.jenis);
+    _tanggalLahirController = TextEditingController(text: widget.hewan.tanggalLahir);
+    _hargaController = TextEditingController(text: widget.hewan.harga.toString());
+    _status = widget.hewan.status;
+  }
+
+  @override
+  void dispose() {
+    _namaController.dispose();
+    _jenisController.dispose();
+    _tanggalLahirController.dispose();
+    _hargaController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +44,10 @@ class _EditHewanPageState extends State<EditHewanPage> {
         backgroundColor: Colors.deepPurple,
         foregroundColor: Colors.white,
       ),
-      body: const Center(child: Text('Form Edit Hewan')),
+      body: Form(
+        key: _formKey,
+        child: const Center(child: Text('Form Edit Hewan')),
+      ),
     );
   }
 }
