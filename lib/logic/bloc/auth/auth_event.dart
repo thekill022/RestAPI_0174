e4bfc2c7373e04fcb33fb0e957b112693204a281
thekill022 <gibfathoni@gmail.com>
2026-05-05 00:56:53 +0700

    membuat auth event

---
+++ lib/logic/bloc/auth/auth_event.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+abstract class AuthEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class AppStarted extends AuthEvent{}
+class LoginRequested extends AuthEvent {
+  final String email, password;
+  LoginRequested(this.email, this.password);
+}
+class RegisterRequested extends AuthEvent {
+  final String username, email, password;
+  RegisterRequested(this.username, this.email, this.password);
+}
+class LogoutRequested extends AuthEvent{}
