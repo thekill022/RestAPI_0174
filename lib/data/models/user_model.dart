e2346ce35b497d3ad8df2c5d037816dfd2b6bd02
thekill022 <gibfathoni@gmail.com>
2026-05-04 23:28:07 +0700

    membuat model user dan juga membuat fungsi untuk konversi data dari json ke object

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,20 @@
+class UserModel {
+  final String id;
+  final String username;
+
+  UserModel({
+    required this.id,
+    required this.username
+});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+        id: json['id'] ?? '',
+        username: json['username'] ?? '',
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, username];
+
+}
