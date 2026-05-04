5fe432d4aca9ffd343a6c3fa825a4ff866f28cae
thekill022 <gibfathoni@gmail.com>
2026-05-05 00:14:22 +0700

    merubah ke storage provider

--- lib/data/repositories/hewan_repository.dart
+++ lib/data/repositories/hewan_repository.dart
@@ -1,4 +1,8 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:restapi0174/data/providers/storage_provider.dart';
+
 class HewanRepository {
   final String baseUrl = 'https://ternak-be-production.up.railway.app/api/v1';
-  final _storage = FlutterSecureStorage();
+  final StorageProvider storage = StorageProvider();
+
 }
